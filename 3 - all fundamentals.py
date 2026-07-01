c315e6cc57d716128004cbd468c5db22cfed06f0
Anilkandpal <anilkandpal2001@gmail.com>
2026-07-01 05:40:41 +0530

    Adds basic file and directory operations in Python
    Implements functionality to display the current working directory, list files, and create/remove directories.
    
    Introduces system information outputs, including Python version and platform details. 
    
    Calculates the sum of two command-line arguments, enhancing usability for basic arithmetic operations.

---
+++ 3 - all fundamentals.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+print(os.getcwd())  # gives the current working directory
+print(
+    os.listdir()
+)  # gives the list of files and directories in the current working directory
+os.mkdir("new_directory")  # creates a new directory in the current working directory
+
+os.rmdir("new_directory")  # removes the directory
+
+print(sys.version)  # gives the version of python installed
+print(sys.platform)  # gives the platform on which python is running
+print(sys.path)  # gives the list of directories where python looks for modules
+print("start")
+# sys.exit()  # exits the program
+print("end")  # this line will not be executed because of sys.exit()
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+print("sum = ", a + b)
